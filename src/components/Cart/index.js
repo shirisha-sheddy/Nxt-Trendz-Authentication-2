d7d156b158dbac5561e420e31c2fb43a3d8d3183
shirisha-sheddy <shirishasheddy963@gmail.com>
2023-10-30 10:02:52 +0000

    application 2 NXT solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,28 @@
+// Write your JS code here
+
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken === undefined) {
+    return <Redirect to="/" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Cart
